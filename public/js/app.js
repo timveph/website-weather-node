422d93071c763de0a00ad997344718320bb44435
pt <54678833+timveph@users.noreply.github.com>
2021-10-23 18:17:37 +0100

    adjusted the request to the correct location

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?location='+encodeURIComponent(searchLocation)).then( (response) => {
+    fetch('/weather?location='+encodeURIComponent(searchLocation)).then( (response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
